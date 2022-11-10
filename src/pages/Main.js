c0d73d556e2d68bb1086ed1c3a264abda1448421
junho2343 <junho234323@gmail.com>
2022-11-10 15:04:37 +0900

    2022-11-10 asfffsafsa

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,11 +2,14 @@ import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import { HiOutlineDocument } from "react-icons/hi";
 import { AiOutlineSearch } from "react-icons/ai";
+import remarkGfm from "remark-gfm";
+
 import Accordion from "../components/Accordion";
 import Content from "../components/Content";
 import AppContext from "../context/AppContext";
 import { getPostOne } from "../common/common.function";
 import PostWrap from "../components/PostWrap";
+import ReactMarkdown from "react-markdown";
 
 function Main() {
   const [selected, setSelected] = useState(null);
@@ -90,7 +93,7 @@ function Main() {
       )}
 
       <RightWrap selected={selected}>
-        <RightHeader>
+        <RightHeader visible={openPost.length !== 0 ? true : false}>
           {openPost.map((one, index) => {
             const data = getPostOne(postData, one);
 
@@ -123,7 +126,10 @@ function Main() {
             );
           })}
         </RightHeader>
-        <RightContent selected={selected}>
+        <RightContent
+          selected={selected}
+          visible={openPost.length !== 0 ? true : false}
+        >
           {(() => {
             const data = getPostOne(postData, selectedPost);
 
@@ -137,11 +143,16 @@ function Main() {
                       <strong>Junho</strong> | {data.data?.date}
                     </p>
                     <div>
-                      {data.data?.tag.map((one, index) => (
+                      {data.data?.tag?.map((one, index) => (
                         <span key={index}>{one}</span>
                       ))}
                     </div>
-                    <div>{data.data?.content}</div>
+                    <div>
+                      <ReactMarkdown
+                        children={data.data?.content}
+                        remarkPlugins={[remarkGfm]}
+                      />
+                    </div>
                   </div>
                 </>
               )
@@ -167,7 +178,7 @@ const RightWrap = styled.div`
 const RightHeader = styled.div`
   width: 100%;
   height: 50px;
-  display: flex;
+  display: ${({ visible }) => (visible ? "flex" : "none")};
   overflow-x: scroll;
   background-color: ${({ theme }) => theme.color.secondary};
 
@@ -209,7 +220,7 @@ const RightHeader = styled.div`
 
 const RightContent = styled.div`
   width: 100%;
-  height: calc(100% - 50px);
+  height: ${({ visible }) => (visible ? "calc(100% - 50px)" : "100%")};
   background-color: ${({ theme }) => theme.color.primary};
   padding: 10px 20px;
 
